4558837863b017db81228347d2990e244cfd32bf
martinvonk <vonk.mart@gmail.com>
2026-01-29 18:32:23 +0100

    last update to the tests

--- .github/workflows/test_notebooks.py
+++ .github/workflows/test_notebooks.py
@@ -3,14 +3,11 @@
 from pathlib import Path
 import papermill as pm
 import pytest
-from shutil import rmtree
 
 #%%
 # paths
 CWD = Path(__file__).parent.resolve()
 BASE_DIR = CWD.parent.parent
-OUTPUT_DIR = CWD / ".ipynb_checkpoints"
-OUTPUT_DIR.mkdir(exist_ok=True)
 
 #%%
 # Collect all notebooks you want to test
@@ -26,25 +23,18 @@ for path in BASE_DIR.iterdir():
             notebooks.append(nb.resolve())
 #%%
 # test function
-def test_notebook_runs(nb_path: Path) -> None:
-    out_path = OUTPUT_DIR / nb_path.name
-    print(f"Testing notebook: {nb_path.name}")
+@pytest.mark.parametrize("path", notebooks, ids=lambda p: str(p))
+def test_notebook_runs(path: Path) -> None:
+    print(f"Testing notebook: {path.name}")
     pm.execute_notebook(
-        input_path=str(nb_path),
-        output_path=str(out_path),
-        kernel_name="python3",
+        input_path=str(path),
+        output_path=None,
         log_output=True,
-        cwd=str(nb_path.parent),
+        kernel_name="python3",
+        cwd=str(path.parent),
     )
 
 #%%
-@pytest.mark.parametrize("nb_path", notebooks, ids=lambda p: str(p))
-def test_notebook_runs_pytest(nb_path: Path) -> None:
-    test_notebook_runs(nb_path=nb_path)
-
 # local test
-# for nb_path in sorted(notebooks):
-#     test_notebook_runs(nb_path)
-#     break
-# %%
-rmtree(OUTPUT_DIR)
+# for path in sorted(notebooks):
+#     test_notebook_runs(path=path)
