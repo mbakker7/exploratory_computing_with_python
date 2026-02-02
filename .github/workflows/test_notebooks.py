49ba7389d5cb7254b4b2d1ffb4e6370f29ae0bbf
martinvonk <vonk.mart@gmail.com>
2026-02-02 20:08:19 +0100

    test all notebooks, no exceptions

--- .github/workflows/test_notebooks.py
+++ .github/workflows/test_notebooks.py
@@ -11,14 +11,9 @@ BASE_DIR = CWD.parent.parent
 
 #%%
 # Collect all notebooks you want to test
-notebooks_to_ignore = ["bugs"]
 notebooks = []
 for path in BASE_DIR.iterdir():
     if path.is_dir():
-        if any(ignored in path.name for ignored in notebooks_to_ignore):
-            continue
-        if ".github" in str(path):
-            continue
         for nb in path.rglob("*_sol.ipynb"):
             notebooks.append(nb.resolve())
 #%%
@@ -26,15 +21,27 @@ for path in BASE_DIR.iterdir():
 @pytest.mark.parametrize("path", notebooks, ids=lambda p: str(p))
 def test_notebook_runs(path: Path) -> None:
     print(f"Testing notebook: {path.name}")
-    pm.execute_notebook(
-        input_path=str(path),
-        output_path=None,
-        log_output=True,
-        kernel_name="python3",
-        cwd=str(path.parent),
-    )
+    if "bugs" in str(path):
+        with pytest.raises(Exception):
+            pm.execute_notebook(
+                input_path=str(path),
+                output_path=None,
+                log_output=True,
+                kernel_name="python3",
+                cwd=str(path.parent),
+            )
+    else:
+        pm.execute_notebook(
+            input_path=str(path),
+            output_path=None,
+            log_output=True,
+            kernel_name="python3",
+            cwd=str(path.parent),
+        )
 
 #%%
 # local test
 # for path in sorted(notebooks):
+#     if "bugs" in str(path):
+#         continue
 #     test_notebook_runs(path=path)
